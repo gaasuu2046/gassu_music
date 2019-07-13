f2eb492aa654b194a887114c0102955a0875bf71
sugawarahr <fh6edpizf@gmail.com>
2019-07-13 17:26:26 +0900

    clientとbackend分離 curl -X GET "http://localhost:8080/v1/rest/airing/{id}"でDynamoDBから値取得可能

--- batch-service/client/client.go
+++ batch-service/client/client.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"net/http"
 
-	"golang.org/x/net/context"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 
 	pb "gassu_music/batch-service/pb"
 	"log"
 )
 
-func main() {
-	//sampleなのでwithInsecure
-	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+func connection() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	endpoint := fmt.Sprint("localhost:8081")
+	err := pb.RegisterAiringHandlerFromEndpoint(ctx, mux, endpoint, opts)
 	if err != nil {
-		log.Fatal("client connection error:", err)
+		log.Fatal("handler registration error:", err)
 	}
-	defer conn.Close()
-	client := pb.NewAiringClient(conn)
-	message := &pb.GetAiringMessage{AiringId: "bkf0rlp94cg7ec81mi1g"}
-	res, err := client.GetAiring(context.TODO(), message)
-	fmt.Printf("result:%#v \n", res)
-	fmt.Printf("error::%#v \n", err)
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+	if err := connection(); err != nil {
+		log.Fatal("connection error", err)
+	}
+	//sampleなのでwithInsecure
+	// conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	// if err != nil {
+	// 	log.Fatal("client connection error:", err)
+	// }
+	// defer conn.Close()
+	// client := pb.NewAiringClient(conn)
+	// message := &pb.GetAiringMessage{AiringId: "bkf0rlp94cg7ec81mi1g"}
+	// res, err := client.GetAiring(context.TODO(), message)
+	// fmt.Printf("result:%#v \n", res)
+	// fmt.Printf("error::%#v \n", err)
 }
--- batch-service/server/server.go
+++ batch-service/server/server.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	listenPort, err := net.Listen("tcp", ":8080")
+	listenPort, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalln(err)
 	}
